206e4c134209fdc10a6e5fd72a640f2844b0882f
t-arae <aroearue@gmail.com>
2022-10-24 13:17:48 +0900

    purrr::pmap

--- R/package_imports.R
+++ R/package_imports.R
@@ -6,7 +6,7 @@ NULL
 
 #' @importFrom tibble tibble as_tibble column_to_rownames
 #' @importFrom purrr
-#'   imap map walk iwalk discard pmap
+#'   imap map walk iwalk discard
 #' @importFrom tidyr pivot_longer replace_na
 #' @importFrom ggplot2
 #'   labs ylab xlab ylim xlim
