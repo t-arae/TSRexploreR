7b49418629a0d027dab1debfc31702102cb37c34
t-arae <aroearue@gmail.com>
2022-10-25 12:21:30 +0900

    suppress Notes

--- R/tss_metrics.R
+++ R/tss_metrics.R
@@ -50,6 +50,7 @@ mark_dominant <- function(
   mark_per="default",
   exclude_antisense=TRUE
 ) {
+  TSR_FHASH <- score <- simple_annotations <- feature_type <- NULL
 
   ## Check inputs.
   assertthat::assert_that(is(experiment, "tsr_explorer"))
